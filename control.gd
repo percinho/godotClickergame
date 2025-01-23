0be18b4f4f19b67f32b97c9accb207e7ba03393d
percinho <percinho@users.noreply.github.com>
2025-01-23 08:57:50 +0000

    Added comments and tidied some variable names

--- control.gd
+++ control.gd
@@ -1,55 +1,70 @@
 class_name Clicker
 extends Control
 
-#TODO Retool the update nodes as clones of PanelContainer
-# but also gather them on ready as an array for updates
+#TODO Add passive income upgrade
+#TODO Could the upgrade be done in the node itself and it just
+# emit a signal with the number of fish to take off and mult to increase?
 
-@export var label : Label
+# Export base variables that need to be updated 
+# This is the main fish counter label
+@export var fishLabel : Label
+# This is the number of fish
 @export var fish: int = 0
+# This is the prestige gain label
 @export var prestigeLabel : Label
-var baseMult: float = 1
-var fishMultiplier: float = 1
-var buttons = 0
-	
+
+# Other base variables
+var baseMult: float = 1 # fish base multiplier, increased on Prestige
+var fishMultiplier: float = 1 # Ongoing fish mult, increased by upgrades
+var upgradeNodes = 0 # used on ready to collect up all the upgrade nodes
+
+# Adds fish to the total
 func add_fish() -> void:
 	fish += fishMultiplier
 
+# Updates any labels in direct child nodes
 func update_label_text() -> void:
-	label.text = "You have %d fish" %fish
-	prestigeLabel.text = "Prestige for %f added multiplier" %calculatePrestige()
+	fishLabel.text = "You have %d fish" %fish
+	prestigeLabel.text = "Prestige for %d added multiplier" %calculatePrestige()
 
+# Returns current prestige multiplier
 func calculatePrestige():
-	var spam = floor(fish / 1000)
-	return spam * 0.5
+	var spam = floor(fish / 1500)
+	return spam
 
+# Carries out the prestige. 
+# Sets the new base mult, sets the finsh mult to match it
+# then resets fish and all upgrade nodes
 func prestige() -> void:
 	baseMult += calculatePrestige()
 	fishMultiplier = baseMult
 	fish = 0
-	for i in buttons:
-		i.reset()
 	update_label_text()
+	for i in upgradeNodes:
+		i.reset()
 	
 # Called when the node enters the scene tree for the first time.
+# Gathers the upgrade nodes for future use
+# Then sets all their labels to display the cost
 func _ready() -> void:
-	print(2616 /  1000)
-	buttons = get_tree().get_nodes_in_group("update_buttons")
-	for i in buttons:
+	upgradeNodes = get_tree().get_nodes_in_group("update_buttons")
+	for i in upgradeNodes:
 		i.updateLabel()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+# Checks each upgrade node to see if the buy button should be enabled
 func _process(delta: float) -> void:
-	for i in buttons:
+	for i in upgradeNodes:
 		i.buyButtonStatus(fish)
-	pass
 
-
+# Button press for the mainn buy fish button
 func _on_button_pressed() -> void:
 	add_fish()
 	update_label_text()
 
-
+# Button press for the Buy button on each upgrade
+# Each node should pass their own path in, plus cost. 
 func _on_node_button_pressed(path, updateNumber) -> void:
 	var thisNode = self.get_node(path)
 	fish -= thisNode.returnCost()
